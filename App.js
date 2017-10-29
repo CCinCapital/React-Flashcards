421ea1cfceb2bf682b45e2a5bab37b5aa3e90b08
Can Chen <canchen@me.com>
2017-10-29 08:57:04 -0400

    import dependencies, create screens for React-Navigation
    - Wrap app in provider

--- App.js
+++ App.js
@@ -1,23 +1,83 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { View, Platform, StatusBar, FlatList, Text } from 'react-native'
+import { createStore } from 'redux'
+import { Provider } from 'react-redux'
+import { Constants } from 'expo'
+import { TabNavigator, StackNavigator, DrawerNavigator } from 'react-navigation'
+
+import HomeScreen from './components/HomeScreen'
+import DeckScreen from './components/DeckScreen'
+import NewDeckScreen from './components/NewDeckScreen'
+import QuizScreen from './components/QuizScreen'
+import NewCardScreen from './components/NewCardScreen'
+
+import reducer from './reducers'
+
+function PhoneStatusBar ({backgroundColor, ...props}) {
+  return (
+    <View style={{ backgroundColor, height: Constants.statusBarHeight }}>
+      <StatusBar translucent backgroundColor={backgroundColor} {...props} />
+    </View>
+  )
+}
+
+const MyDecksTab = TabNavigator(
+{ // Route Configs
+  MyDecks: { 
+    screen: HomeScreen,
+    navigationOptions: {
+      tabBarLabel: 'My Decks'
+    },
+  },
+  NewDeckScreen: { 
+    screen: NewDeckScreen,
+    navigationOptions: {
+      tabBarLabel: 'New Deck'
+    },
+  },
+}, 
+{ // Navigator Configs
+  // tabBarPosition: 'bottom',  
+  navigationOptions: {
+    header: null
+  },
+  tabBarOptions: {
+    activeTintColor: '#fff',
+  }
+})
+
+const RootNav = StackNavigator(
+{
+  Home: { 
+    screen: MyDecksTab,
+  },
+  DeckScreen: { 
+    screen: DeckScreen,
+  },
+  NewCardScreen: {
+    screen: NewCardScreen,
+    navigationOptions: {
+      header: null
+    }
+  },
+  QuizScreen: {
+    screen: QuizScreen,
+  }
+},
+{
+  initialRouteName: 'Home',
+})
 
 export default class App extends React.Component {
   render() {
     return (
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-        <Text>Changes you make will automatically reload.</Text>
-        <Text>Shake your phone to open the developer menu.</Text>
-      </View>
+      <Provider store={createStore(reducer)}>
+        <View style={{flex: 1}}>
+          <PhoneStatusBar backgroundColor='#fff' barStyle='light-content' />
+          <RootNav />
+        </View>
+      </Provider>
     );
   }
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
