5af3126e49e447220ec960cbd4e9e0570fcf4ab3
Can Chen <canchen@me.com>
2017-10-28 23:38:09 -0400

    update Deck to Pure component

--- components/Deck.js
+++ components/Deck.js
@@ -1,38 +1,13 @@
 import React, { Component } from 'react'
-import { connect } from 'react-redux'
-
-import { NavigationActions } from 'react-navigation'
 import { TouchableOpacity, View, Text, StyleSheet } from 'react-native'
 
-import { activateDeck } from '../actions'
-
-class Deck extends Component {
-
-  handleClick = (e) => {
-    const deck = this.props.deck
-
-    this.props.dispatch(activateDeck({
-      deck,
-    }))
-
-    // this.toDeck()
-  }
-
-  // toDeck = () => {
-  //   const navigateAction = NavigationActions.navigate({
-  //     routeName: 'DeckScreen'
-  //   })
-  //   this.props.navigation.dispatch(navigateAction)
-  // }
-
-  render() {
-    return (
-      <TouchableOpacity onPress={this.handleClick} style={styles.deck}>
-        <Text style={styles.title}>{this.props.deck[1].title}</Text>
-        <Text style={styles.discr}>{this.props.deck[1].cards.length} cards</Text>
-      </TouchableOpacity>
-    )
-  }
+export default function Deck ({ onPress, deck}) {
+  return (
+    <TouchableOpacity onPress={onPress} style={styles.deck}>
+      <Text style={styles.title}>{deck.title}</Text>
+      <Text style={styles.discr}>{deck.cards.length} cards</Text>
+    </TouchableOpacity>
+  )
 }
 
 const styles = StyleSheet.create({
@@ -54,5 +29,4 @@ const styles = StyleSheet.create({
   }
 })
 
-export default connect()(Deck)
 
