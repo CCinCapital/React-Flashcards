80f78553d55f9778874d3a4efe52161103138edf
Can Chen <canchen@me.com>
2017-10-30 05:46:33 -0400

    remove extra `s` in `scheduleLocalNotificationAsync` name

--- utils/helpers.js
+++ utils/helpers.js
@@ -34,7 +34,7 @@ export function setLocalNotification () {
               tomorrow.setHours(20)
               tomorrow.setMinutes(0)
 
-              Notifications.scheduleLocalNotificationsAsync(
+              Notifications.scheduleLocalNotificationAsync(
                 createNotification(),
                 {
                   time: tomorrow,
